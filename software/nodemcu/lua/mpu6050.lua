5a14ae2c8cef2c05469ac564393a8171cb69ecee
feilong <feilongphone@gmail.com>
2018-11-05 19:20:48 +0800

    save

--- software/nodemcu/lua/mpu6050.lua
+++ software/nodemcu/lua/mpu6050.lua
@@ -13,15 +13,18 @@ function read_reg(id, dev_addr, reg_addr)
         return nil; 
     end 
     i2c.write(id, reg_addr)
+
     i2c.stop(id)
     i2c.start(id)
-    i2c.address(id, dev_addr, i2c.RECEIVER)
-    --if i2c.address(id, dev_addr, i2c.RECEIVER) == false then
-    --    i2c.stop(id)
-    --    return nil; 
-    --end 
+    if i2c.address(id, dev_addr, i2c.RECEIVER) == false then
+        i2c.stop(id)
+        print(11,gpio.read(1),gpio.read(2))
+        return nil; 
+    end 
     c = i2c.read(id, 1)
     i2c.stop(id)
+
+    return c
 end
 
 function set_reg(id, dev_addr, reg_addr, val)
@@ -35,32 +38,39 @@ function set_reg(id, dev_addr, reg_addr, val)
     i2c.stop(id)
 end
 
-i2c.setup(IICID, IICSDA, IICSCL, i2c.SLOW)
---set_reg(IICID, 0x68, 0x6B, 0x00);
-hmc5883l.setup()
-function readMpu()
-local x,y,z = hmc5883l.read()
-print(string.format("x = %d, y = %d, z = %d", x, y, z))
-
-for i = 1,0x7f do
-    reg = read_reg(IICID, 0x1e, 0x04)
-    --reg = read_reg(IICID, 0x68, 0x75)
-    if reg == nil then 
-        debugMsg("unable connect to gyro!")
-        else
-        print(i)
+function i2cScan()
+    for i = 0x00,0x7f do
+        i2c.start(0)
+        print(i, i2c.address(0, i, i2c.TRANSMITTER))
+        i2c.stop(0)
     end
 end
 
-    reg = read_reg(IICID, 0x1e, 0x04)
-    --reg = read_reg(IICID, 0x68, 0x75)
-    if reg == nil then 
-        debugMsg("unable connect to gyro!")
-        mputimer:start()
-        return
-    end
-    print(string.byte(reg))
+function i2cDump(id, dev_addr)
+    for i = 0x0d,0x75 do
+        r = read_reg(id, dev_addr, i)
+        if r ~= nil then
+            print(i," -> ", string.byte(r))
+        else
+            print(i, "err")
+        end
+        --print(gpio.read(1),gpio.read(2))
 
+        tmr.delay(100)
+    end
+end
+
+i2c.setup(IICID, IICSDA, IICSCL, i2c.SLOW)
+gpio.mode(IICSDA, gpio.OUTPUT, gpio.FLOAT)
+gpio.mode(IICSCL, gpio.OUTPUT, gpio.FLOAT)
+set_reg(IICID, 0x68, 0x6B, 0x00);
+--mpu6050.setup()
+function readMpu()
+    --local ax,ay,az,temp,gx,gy,gz = mpu6050.read()
+    --print(string.format(
+    --        "ax = %d, ay = %d, az = %d, temp = %d, gx = %d, gy = %d, gz = %d", 
+    --        ax, ay, az, temp, gx, gy, gz))
+    --print(read_reg(0,0x68,0x75))
     mputimer:start()
 end
 
@@ -68,5 +78,5 @@ if mputimer == nil then
     mputimer = tmr.create()
 end
 mputimer:alarm(1000, tmr.ALARM_SEMI, readMpu)
-mputimer:start()
+--mputimer:start()
 
